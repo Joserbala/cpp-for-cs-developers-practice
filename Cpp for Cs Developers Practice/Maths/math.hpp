c90ff2f87d8f818b2c9c25f034c476fd97bcadb1
José Gala Naranjo <jgalanar@gmail.com>
2022-03-26 13:28:02 +0100

    Add documentation structure example

--- Cpp for Cs Developers Practice/Maths/math.hpp
+++ Cpp for Cs Developers Practice/Maths/math.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+/// <summary>
+/// Sums two numbers.
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
 float Add(float a, float b);
 
 const float PI = 3.14f;
--- Cpp for Cs Developers Practice/Maths/vector.hpp
+++ Cpp for Cs Developers Practice/Maths/vector.hpp
@@ -2,4 +2,12 @@
 
 #include "math.hpp"
 
+/// <summary>
+/// Performs the dot product of two 2D vectors.
+/// </summary>
+/// <param name="aX"></param>
+/// <param name="aY"></param>
+/// <param name="bX"></param>
+/// <param name="bY"></param>
+/// <returns></returns>
 float Dot(float aX, float aY, float bX, float bY);
