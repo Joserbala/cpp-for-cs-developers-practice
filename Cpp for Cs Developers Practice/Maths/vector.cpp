88a574f910e9e6f817df7d02ed47bd3c519ace09
José Gala Naranjo <jgalanar@gmail.com>
2022-03-26 13:14:53 +0100

    Change place of include at vector files

--- Cpp for Cs Developers Practice/Maths/vector.cpp
+++ Cpp for Cs Developers Practice/Maths/vector.cpp
@@ -1,5 +1,4 @@
 #include "vector.hpp"
-#include "math.hpp"
 
 float Dot(float aX, float aY, float bX, float bY) {
 	return Add(aX * bX, aY * bY);
--- Cpp for Cs Developers Practice/Maths/vector.hpp
+++ Cpp for Cs Developers Practice/Maths/vector.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
+#include "math.hpp"
+
 float Dot(float aX, float aY, float bX, float bY);
