d978d1fa3116968895db9dd7a7f80cd5afab5579
José Gala Naranjo <jgalanar@gmail.com>
2022-03-26 12:35:12 +0100

    Add vector files

---
+++ Cpp for Cs Developers Practice/Maths/vector.cpp
@@ -0,0 +1,5 @@
+#include "vector.hpp"
+
+float Dot(float aX, float aY, float bX, float bY) {
+	return aX * bX + aY * bY;
+}
---
+++ Cpp for Cs Developers Practice/Maths/vector.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+float Dot(float aX, float aY, float bX, float bY);
